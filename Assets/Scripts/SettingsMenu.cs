92838d717fd7d84c76e5bccf43f95b14758aa152
Cham <chamarasilva55@gmail.com>
2020-11-20 21:49:13 +0000

    resolution & fullscreen options in settings
    added a dropdown for the resolution and a checkbox for the fullscreen options.
    
    created a settings menu script in order for these options to work. attached script as a component to the canvas in order to have an object reference to be able to use the methods as defined in the script.
    
    not entirely sure if the options work 100% as they should

---
+++ Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+
+    public Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //initialise array with all possible resolutions
+        resolutions = Screen.resolutions;
+
+        //ensure there's nothing already in the dropdown
+        resolutionDropdown.ClearOptions();
+
+        //create a list of strings that will hold the options for resolution
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        //creating the string containing the width & height for each resolution
+        //then adding to list
+        for (int i = 0; i < resolutions.Length; i ++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+
+            //finding the current resolution of window
+            if (resolutions[i].width == Screen.width &&
+                resolutions[i].height == Screen.height) 
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        //updating the dropdown
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void setResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void setFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+}
