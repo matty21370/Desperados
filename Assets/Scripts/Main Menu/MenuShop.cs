1bd7bcc928b8c785ba8c0235e4fb0f055ee7bbeb
matty21370 <matthewburke01@hotmail.co.uk>
2020-11-22 17:32:03 +0000

    Added an item to the menu shop

---
+++ Assets/Scripts/Main Menu/MenuShop.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuShop : MonoBehaviour
+{
+    [SerializeField] private GameObject description;
+    [SerializeField] private Text itemTitle;
+    [SerializeField] private Image itemImage;
+    [SerializeField] private Text itemDescription;
+
+    private MenuShopItem selectedItem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        description.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SelectItem(MenuShopItem item)
+    {
+        this.selectedItem = item;
+        description.SetActive(true);
+        itemTitle.text = item.name;
+        itemImage.sprite = item.icon;
+        itemDescription.text = item.description;
+    }
+
+    public void PurchaseItem()
+    {
+        print("Purchased " + itemTitle);
+    }
+}
---
+++ Assets/Scripts/Main Menu/MenuShopItem.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuShopItem : MonoBehaviour
+{
+    public string name;
+    public int price;
+    public Sprite icon;
+    public string description;
+}
