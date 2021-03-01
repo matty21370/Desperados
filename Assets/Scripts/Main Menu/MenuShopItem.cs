93035f1128073ef4548c911f7a7db2af820dc670
AndyViney <190063731@aston.ac.uk>
2021-03-01 17:24:48 +0000

    start of cosmetic shop

--- Assets/Scripts/Main Menu/MenuShopItem.cs
+++ Assets/Scripts/Main Menu/MenuShopItem.cs
@@ -1,11 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class MenuShopItem : MonoBehaviour
 {
     public string name;
     public int price;
     public Sprite icon;
     public string description;
-}
+    [SerializeField]private TMP_Text levelText;
+
+    public void SetText(string text)
+    {
+		if (text.Equals("level10"))
+		{
+			if (checkLevel(10))
+			{
+                Debug.Log("10");
+			}
+		}
+
+    }
+
+
+    private bool checkLevel(int num)
+	{
+        int min = int.Parse(levelText.text);
+        if (min <= num)
+		{
+            return true;
+		}
+		else
+		{
+            return false;
+		}
+
+    }
+    }
