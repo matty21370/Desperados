ddb513959282b092af4871b9efcb299bc504d2f5
matty21370 <matthewburke01@hotmail.co.uk>
2021-03-03 16:34:28 +0000

    Obstacle position fix

--- Assets/Scripts/Game Mechanics/RandomPosition.cs
+++ Assets/Scripts/Game Mechanics/RandomPosition.cs
@@ -1,14 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class RandomPosition : MonoBehaviour
+public class RandomPosition : MonoBehaviourPunCallbacks
 {
     public Vector3[] positions;
     void Start()
     {
+        if (FindObjectsOfType<Player>().Length == 1)
+        {
+            photonView.RPC(nameof(SetPosition), RpcTarget.AllBuffered);
+        }
+    }
+
+    [PunRPC]
+    private void SetPosition()
+    {
         int randomNumber = Random.Range(0, positions.Length);
         transform.position = positions[randomNumber];
-
     }
 }
